a3341e04edf284dc7911909aa958c7df1942b9b7
Luiz Miguel Veloso <luizzmiguel17@gmail.com>
2024-06-13 22:19:17 -0300

    script

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+let tela = ""
 
+function somar(a, b){ //
+  return (a + b)
+}
+function subtrair(a, b){ // a menos b
+  return (a - b)
+}
+function multiplicar(a, b){
+  return (a * b)
+}
+function dividir(a, b){
+  return (a / b)
+}
+
+function checar(){
+  
+}
